93acefc69419ae508c9f6bb79be333161fa51c67
Arrotanis <frantisek.bilek@uhk.cz>
2024-04-20 15:55:50 +0200

    cors funguje

---
+++ microservices/api-gateway/src/main/java/com/mois/apigateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.mois.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/*@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Use addAllowedOriginPattern("*") for patterns
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L); // Optional: set the max age to reduce pre-flight requests
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
+*/
