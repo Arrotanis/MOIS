ef01b68774d602c68a3ddc27cf19826fbe3beb83
pokorpe <117550576+pokorpe@users.noreply.github.com>
2024-04-23 04:24:39 +0200

    Frontend prog

--- bank-spa/src/Accounts.js
+++ bank-spa/src/Accounts.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { TextField, Button, CircularProgress, Typography, List, ListItem, ListItemText, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
-/*const Accounts = () => {
+const Accounts = ({ loggedIn, onLogin }) => {
     const [accountData, setAccountData] = useState(null);
     const [accountId, setAccountId] = useState('');
     const [loading, setLoading] = useState(false);
-    const [addBalanceAmount, setAddBalanceAmount] = useState(0);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedAccount, setSelectedAccount] = useState(null);
 
     const sendLoginRequest = () => {
         setLoading(true);
@@ -13,6 +15,7 @@ import axios from 'axios';
             .then(response => {
                 setAccountData(response.data);
                 setLoading(false);
+                onLogin();
             })
             .catch(error => {
                 console.error('Error retrieving account data:', error);
@@ -20,194 +23,66 @@ import axios from 'axios';
             });
     };
 
-    const addBalance = () => {
-        setLoading(true);
-        axios.post('http://localhost:8080/api/transaction/add-balance', {
-            id: accountId,
-            addBalanceAmount: addBalanceAmount
-        })
-            .then(response => {
-                console.log('Balance added successfully:', response.data);
-                sendLoginRequest(); // Refresh account data after balance addition
-            })
-            .catch(error => {
-                console.error('Error adding balance:', error);
-                setLoading(false);
-            });
+    const handleAccountClick = (account) => {
+        setSelectedAccount(account);
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
     };
 
     return (
-        <>
-            <div>
-                <label htmlFor="account">Enter your account ID: </label>
-                <input type="text" id="account" value={accountId} onChange={(e) => setAccountId(e.target.value)} />
-                <button onClick={sendLoginRequest} disabled={loading}>Get Account Data</button>
-            </div>
-
-            {loading && <p>Loading...</p>}
-
-            {accountData && (
+        <div>
+            {!loggedIn && (
+                <>
+                    <TextField
+                        id="account"
+                        label="Login (ID)"
+                        value={accountId}
+                        onChange={(e) => setAccountId(e.target.value)}
+                        fullWidth
+                        variant="outlined"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={sendLoginRequest}
+                        disabled={loading}
+                        sx={{ marginTop: '1rem' }}
+                    >
+                        {loading ? <CircularProgress size={24} /> : 'Login'}
+                    </Button>
+                </>
+            )}
+            {loggedIn && accountData && (
                 <div>
-                    <h2>Account Information</h2>
-                    {accountData.map(account => (
-                        <div key={account.id}>
-                            <p>Account ID: {account.id}</p>
-                            <p>Owner Profile ID: {accountId}</p>
-                            <p>Deposited Balance: {account.balance}</p>
-                            <input type="number" value={addBalanceAmount} onChange={(e) => setAddBalanceAmount(e.target.value)} />
-                            <button onClick={addBalance} disabled={loading}>Add Balance</button>
-                        </div>
-                    ))}
+                    <Typography variant="h6">Accounts</Typography>
+                    <List>
+                        {accountData.map(account => (
+                            <ListItem key={account.id} button onClick={() => handleAccountClick(account)}>
+                                <ListItemText primary={`Account number: ${account.id}`} />
+                                <ListItemText primary={`Balance: ${account.balance}`} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Dialog open={openDialog} onClose={handleCloseDialog}>
+                        <DialogTitle>Account Details</DialogTitle>
+                        <DialogContent>
+                            <Typography variant="subtitle1">Account number: {selectedAccount && selectedAccount.id}</Typography>
+                            <Typography variant="subtitle1">Balance: {selectedAccount && selectedAccount.balance}</Typography>
+                            {/* TODO historie transakci */}
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleCloseDialog} color="primary">Close</Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
             )}
 
-            {!loading && !accountData && <p>No account data available.</p>}
-        </>
-    );
-};*/
-
-/*const Accounts = () => {
-    const [accountData, setAccountData] = useState(null);
-    const [accountId, setAccountId] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [addBalanceAmount, setAddBalanceAmount] = useState(0);
-    const [selectedAccountId, setSelectedAccountId] = useState(null);
-
-    const sendLoginRequest = () => {
-        setLoading(true);
-        axios.get('http://localhost:8080/api/transaction/accounts/' + accountId)
-            .then(response => {
-                setAccountData(response.data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error retrieving account data:', error);
-                setLoading(false);
-            });
-    };
-
-    const handleAddBalance = () => {
-        setLoading(true);
-        axios.post('http://localhost:8080/api/transaction/add-balance', {
-            targetAccountId: selectedAccountId, // Use selectedAccountId instead of accountId
-            addBalanceAmount: addBalanceAmount
-        })
-            .then(response => {
-                console.log('Balance added successfully:', response.data);
-                setLoading(false);
-                sendLoginRequest(); // Refresh account data after balance addition
-            })
-            .catch(error => {
-                console.error('Error adding balance:', error);
-                setLoading(false);
-            });
-    };
-
-    return (
-        <>
-            <div>
-                <label htmlFor="account">Enter your account ID: </label>
-                <input type="text" id="account" value={accountId} onChange={(e) => setAccountId(e.target.value)} />
-                <button onClick={sendLoginRequest} disabled={loading}>Get Account Data</button>
-            </div>
-
-            {loading && <p>Loading...</p>}
-
-            {accountData && (
-                <div>
-                    <h2>Account Information</h2>
-                    {accountData.map(account => (
-                        <div key={account.id}>
-                            <p>Account ID: {account.id}</p>
-                            <p>Owner Profile ID: {accountId}</p>
-                            <p>Deposited Balance: {account.balance}</p>
-                            <input type="number" value={addBalanceAmount} onChange={(e) => setAddBalanceAmount(e.target.value)} />
-                            <button onClick={() => { setSelectedAccountId(account.id); handleAddBalance(); }} disabled={loading}>Add Balance</button>
-
-                        </div>
-                    ))}
-                </div>
-            )}
-
-            {!loading && !accountData && <p>No account data available.</p>}
-        </>
-    );
-};*/
-const Accounts = () => {
-    const [accountData, setAccountData] = useState(null);
-    const [accountId, setAccountId] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [balanceAmounts, setBalanceAmounts] = useState({}); // State to store balance amounts for each account
-    const [selectedAccountId, setSelectedAccountId] = useState(null);
-
-    const sendLoginRequest = () => {
-        setLoading(true);
-        axios.get('http://localhost:8080/api/transaction/accounts/' + accountId)
-            .then(response => {
-                setAccountData(response.data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error retrieving account data:', error);
-                setLoading(false);
-            });
-    };
-
-    const handleAddBalance = (accountId) => {
-        setLoading(true);
-        axios.post('http://localhost:8080/api/transaction/add-balance', {
-            targetAccountId: accountId, // Use accountId instead of selectedAccountId
-            addBalanceAmount: balanceAmounts[accountId] // Get balance amount from state for the specific account
-        })
-            .then(response => {
-                console.log('Balance added successfully:', response.data);
-                setLoading(false);
-                sendLoginRequest(); // Refresh account data after balance addition
-            })
-            .catch(error => {
-                console.error('Error adding balance:', error);
-                setLoading(false);
-            });
-    };
-
-    const handleBalanceChange = (accountId, value) => {
-        // Update the balance amount for the specific account
-        setBalanceAmounts(prevState => ({
-            ...prevState,
-            [accountId]: value
-        }));
-    };
-
-    return (
-        <>
-            <div>
-                <label htmlFor="account">Enter your account ID: </label>
-                <input type="text" id="account" value={accountId} onChange={(e) => setAccountId(e.target.value)} />
-                <button onClick={sendLoginRequest} disabled={loading}>Get Account Data</button>
-            </div>
-
-            {loading && <p>Loading...</p>}
-
-            {accountData && (
-                <div>
-                    <h2>Account Information</h2>
-                    {accountData.map(account => (
-                        <div key={account.id}>
-                            <p>Account ID: {account.id}</p>
-                            <p>Owner Profile ID: {accountId}</p>
-                            <p>Deposited Balance: {account.balance}</p>
-                            <input type="number" value={balanceAmounts[account.id] || ''} onChange={(e) => handleBalanceChange(account.id, e.target.value)} />
-                            {/* Enable the button only when balanceAmounts[account.id] is not null */}
-                            <button onClick={() => { setSelectedAccountId(account.id); handleAddBalance(account.id); }} disabled={loading || !balanceAmounts[account.id]}>Add Balance</button>
-                        </div>
-                    ))}
-                </div>
-            )}
-
-            {!loading && !accountData && <p>No account data available.</p>}
-        </>
+            {!loading && !accountData && !loggedIn && <p></p>}
+        </div>
     );
 };
-
 
 export default Accounts;
--- bank-spa/src/App.js
+++ bank-spa/src/App.js
@@ -1,44 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import CreateAccountForm from "./CreateAccountForm";
+import { AppBar, Toolbar, Typography, Container, Button, Grid } from '@mui/material';
 import Accounts from "./Accounts";
+import React, { useState } from 'react';
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
 
+    const handleLogin = () => {
+        setLoggedIn(true);
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Create Account</h1>
-        <div>
-          <a
-              href="http://localhost:8083/login"
-              className="btn btn-lg btn-primary"
-          >
-            <i className="fab fa-google"></i> Continue with Google or Facebook <i className="fab fa-facebook"></i>
-          </a>
+    const handleLogout = () => {
+        setLoggedIn(false);
+    };
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>Banking system</Typography>
+                    {loggedIn && (
+                        <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <Container sx={{ marginTop: '2rem', textAlign: 'center' }}>
+                <Grid container spacing={3} justifyContent="center">
+                    <Grid item xs={12} sm={6}>
+                        <Accounts loggedIn={loggedIn} onLogin={handleLogin} />
+                    </Grid>
+                </Grid>
+            </Container>
+            {!loggedIn && (
+                <Container sx={{ textAlign: 'center', marginTop: '1rem' }}>
+                    <Button variant="contained" color="primary" href="http://localhost:8083/login">
+                        Login with Google or Facebook
+                    </Button>
+                </Container>
+            )}
         </div>
-        <Accounts />
-
-        <CreateAccountForm />
-
-
-        <p>
-
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
