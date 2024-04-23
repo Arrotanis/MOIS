006214f27cfb33762f828a7aac5036cdfa4d7c74
pokorpe <117550576+pokorpe@users.noreply.github.com>
2024-04-24 01:21:56 +0200

    Transaction service fix

--- microservices/transaction-service/src/main/java/com/mois/transactionservice/model/Transaction.java
+++ microservices/transaction-service/src/main/java/com/mois/transactionservice/model/Transaction.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
