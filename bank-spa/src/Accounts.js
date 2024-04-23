e4c7938c0ffe5785aa4280ce5a40fff1e1c9605f
pokorpe <117550576+pokorpe@users.noreply.github.com>
2024-04-23 08:01:34 +0200

    Frontend add balance

--- bank-spa/src/Accounts.js
+++ bank-spa/src/Accounts.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import { TextField, Button, CircularProgress, Typography, List, ListItem, ListItemText, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import {
+    TextField,
+    Button,
+    CircularProgress,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions
+} from '@mui/material';
 
-const Accounts = ({ loggedIn, onLogin }) => {
+const Accounts = ({loggedIn, onLogin}) => {
     const [accountData, setAccountData] = useState(null);
     const [accountId, setAccountId] = useState('');
     const [loading, setLoading] = useState(false);
+    const [balanceAmounts, setBalanceAmounts] = useState({});
+    const [selectedAccountId, setSelectedAccountId] = useState(null);
     const [openDialog, setOpenDialog] = useState(false);
-    const [selectedAccount, setSelectedAccount] = useState(null);
 
     const sendLoginRequest = () => {
         setLoading(true);
@@ -24,12 +37,36 @@ const Accounts = ({ loggedIn, onLogin }) => {
     };
 
     const handleAccountClick = (account) => {
-        setSelectedAccount(account);
+        setSelectedAccountId(account);
         setOpenDialog(true);
     };
 
     const handleCloseDialog = () => {
         setOpenDialog(false);
+    };
+
+    const handleAddBalance = () => {
+        setLoading(true);
+        axios.post('http://localhost:8080/api/transaction/add-balance', {
+            targetAccountId: selectedAccountId.id,
+            addBalanceAmount: balanceAmounts[selectedAccountId.id]
+        })
+            .then(response => {
+                console.log('Balance added successfully:', response.data);
+                setLoading(false);
+                sendLoginRequest();
+            })
+            .catch(error => {
+                console.error('Error adding balance:', error);
+                setLoading(false);
+            });
+    };
+
+    const handleBalanceChange = (accountId, value) => {
+        setBalanceAmounts(prevState => ({
+            ...prevState,
+            [accountId]: value
+        }));
     };
 
     return (
@@ -49,9 +86,9 @@ const Accounts = ({ loggedIn, onLogin }) => {
                         color="primary"
                         onClick={sendLoginRequest}
                         disabled={loading}
-                        sx={{ marginTop: '1rem' }}
+                        sx={{marginTop: '1rem'}}
                     >
-                        {loading ? <CircularProgress size={24} /> : 'Login'}
+                        {loading ? <CircularProgress size={24}/> : 'Login'}
                     </Button>
                 </>
             )}
@@ -61,19 +98,30 @@ const Accounts = ({ loggedIn, onLogin }) => {
                     <List>
                         {accountData.map(account => (
                             <ListItem key={account.id} button onClick={() => handleAccountClick(account)}>
-                                <ListItemText primary={`Account number: ${account.id}`} />
-                                <ListItemText primary={`Balance: ${account.balance}`} />
+                                <ListItemText primary={`Account number: ${account.id}`}/>
+                                <ListItemText primary={`Balance: ${account.balance}`}/>
                             </ListItem>
                         ))}
                     </List>
                     <Dialog open={openDialog} onClose={handleCloseDialog}>
                         <DialogTitle>Account Details</DialogTitle>
                         <DialogContent>
-                            <Typography variant="subtitle1">Account number: {selectedAccount && selectedAccount.id}</Typography>
-                            <Typography variant="subtitle1">Balance: {selectedAccount && selectedAccount.balance}</Typography>
-                            {/* TODO historie transakci */}
+                            <Typography variant="subtitle1">Account
+                                number: {selectedAccountId && selectedAccountId.id}</Typography>
+                            <Typography
+                                variant="subtitle1">Balance: {selectedAccountId && selectedAccountId.balance}</Typography>
+                            <TextField
+                                type="number"
+                                label="Add Balance Amount"
+                                value={balanceAmounts[selectedAccountId.id] || ''}
+                                onChange={(e) => handleBalanceChange(selectedAccountId.id, e.target.value)}
+                                fullWidth
+                                variant="outlined"
+                                sx={{marginTop: '1rem'}}
+                            />
                         </DialogContent>
                         <DialogActions>
+                            <Button onClick={handleAddBalance} color="primary">Add Balance</Button>
                             <Button onClick={handleCloseDialog} color="primary">Close</Button>
                         </DialogActions>
                     </Dialog>
